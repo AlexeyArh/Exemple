5b69f728bbdc3f302f05eef5b0fd9cea1b12d470
Alex <spysfox@mail.ru>
2022-10-09 22:26:44 +0300

    Создал Привет имя

--- Exemp02/Program.cs
+++ Exemp02/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("HiHello, World!");
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
